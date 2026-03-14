f69be8009ddd68444bccea1bac6ff449e428672b
Salhi-Yassine <boy.yassine@hotmail.com>
2026-03-14 04:48:46 +0000

    feat: Add Doctrine Fixtures Bundle, PWA font and validation dependencies, and Vercel React best practices skill.

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -17,4 +17,5 @@ return [
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
